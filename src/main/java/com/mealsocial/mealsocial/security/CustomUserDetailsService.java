6c8f5618b0b4e30afff8a49af22d8772384101cf
Thilina2926 <thilinadilshan2926@gmail.com>
2025-04-18 14:22:13 +0530

    docs(api): Add Swagger documentation

---
+++ src/main/java/com/mealsocial/mealsocial/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.mealsocial.mealsocial.security;
+
+
+import com.mealsocial.mealsocial.domain.User;
+import com.mealsocial.mealsocial.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if(user == null){
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
