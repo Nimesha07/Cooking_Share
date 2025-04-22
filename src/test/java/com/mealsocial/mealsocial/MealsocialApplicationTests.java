d304d329a78bb5736b5560ff94d23ef84186c53b
Maleewij65 <maleeshawijerathne65@gmail.com>
2025-04-22 11:11:00 +0530

    docs: Add README section for backend setup

---
+++ src/test/java/com/mealsocial/mealsocial/MealsocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mealsocial.mealsocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealsocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
