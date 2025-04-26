09f069cbc414c6aff2b72be2ae2651928d5a1da5
Thilina2926 <thilinadilshan2926@gmail.com>
2025-04-26 12:21:52 +0530

    refactor(service): Optimize user service methods

---
+++ src/main/java/com/mealsocial/mealsocial/dto/request/CategoryRequestDto.java
@@ -0,0 +1,12 @@
+package com.mealsocial.mealsocial.dto.request;
+
+import lombok.Data;
+
+@Data
+public class CategoryRequestDto {
+
+
+    private Long categoryId;
+
+    private String categoryName;
+}
